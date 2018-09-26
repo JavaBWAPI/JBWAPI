0acb252a11c0aa2799a436426e4b4112c15d1eff
Jasper Geurtz <jcgeurtz@gmail.com>
2018-09-26 21:57:05 +0200

    add remaining events

--- src/main/java/bwapi/EventHandler.java
+++ src/main/java/bwapi/EventHandler.java
@@ -7,10 +7,12 @@ import bwapi.point.Position;
 class EventHandler implements Client.EventHandler {
     private final BWListener eventListener;
     private Game game;
+    private Client.GameData data;
 
     public EventHandler(final BWListener eventListener, final Client.GameData data) {
         this.eventListener = eventListener;
         game = new Game(data);
+        this.data = data;
     }
 
     public void operation(Client.GameData.Event event) {
@@ -27,14 +29,13 @@ class EventHandler implements Client.EventHandler {
                 break;
             //case 3: //MenuFrame
             case 4: //SendText
-                //TODO eventListener.onSendText();
+                eventListener.onSendText(data.eventString(event.v1()));
                 break;
             case 5: //ReceiveText
-                //TODO eventListener.onReceiveText();
+                eventListener.onReceiveText(game.getPlayer(event.v1()), data.eventString(event.v2()));
                 break;
             case 6: //PlayerLeft
                 eventListener.onPlayerLeft(game.getPlayer(event.v1()));
-                //TODO remove the player from the Game?
                 break;
             case 7: //NukeDetect
                 eventListener.onNukeDetect(new Position(event.v1(), event.v2()));
@@ -66,7 +67,7 @@ class EventHandler implements Client.EventHandler {
                 eventListener.onUnitRenegade(game.getUnit((event.v1())));
                 break;
             case 16: //SaveGame
-                //TODO eventListener.onSaveGame();
+                eventListener.onSaveGame(data.eventString(event.v1()));
                 break;
             case 17: //UnitComplete
                 eventListener.onUnitComplete(game.getUnit((event.v1())));
