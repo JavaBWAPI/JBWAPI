287a6c51e2f5971c138703fd4b035202203ddcc4
bytekeeper <therealdante@gmail.com>
2018-12-27 21:42:08 +0100

    Formatting.

--- src/main/java/bwapi/Buffers.java
+++ src/main/java/bwapi/Buffers.java
@@ -7,32 +7,33 @@ import java.nio.charset.CharsetEncoder;
 import java.nio.charset.StandardCharsets;
 
 final class Buffers {
-  private static final CharsetEncoder enc = Charset.forName("ISO-8859-1").newEncoder();
 
-  private Buffers() {
-    // Utility class
-  }
+    private static final CharsetEncoder enc = Charset.forName("ISO-8859-1").newEncoder();
 
-
-  public static String toString(ByteBuffer buffer, int offset, int maxLen) {
-    final byte[] buf = new byte[maxLen];
-    buffer.position(offset);
-    buffer.get(buf, 0, maxLen);
-    buffer.position(0);
-    int len = 0;
-    while (len < maxLen && buf[len] != 0) {
-      ++len;
+    private Buffers() {
+        // Utility class
     }
-    return new String(buf, 0, len, StandardCharsets.ISO_8859_1);
-  }
 
-  public static void fromString(ByteBuffer buffer, int offset, int maxLen, String s) {
-    final int len = s.length() + 1;
-    if (len >= maxLen) {
-      throw new StringIndexOutOfBoundsException();
+
+    public static String toString(ByteBuffer buffer, int offset, int maxLen) {
+        final byte[] buf = new byte[maxLen];
+        buffer.position(offset);
+        buffer.get(buf, 0, maxLen);
+        buffer.position(0);
+        int len = 0;
+        while (len < maxLen && buf[len] != 0) {
+            ++len;
+        }
+        return new String(buf, 0, len, StandardCharsets.ISO_8859_1);
     }
-    buffer.position(offset);
-    enc.encode(CharBuffer.wrap(s), buffer, true);
-    buffer.rewind();
-  }
+
+    public static void fromString(ByteBuffer buffer, int offset, int maxLen, String s) {
+        final int len = s.length() + 1;
+        if (len >= maxLen) {
+            throw new StringIndexOutOfBoundsException();
+        }
+        buffer.position(offset);
+        enc.encode(CharBuffer.wrap(s), buffer, true);
+        buffer.rewind();
+    }
 }
