72c4219a1421dbcbe0d549987d50f6f8b7370d1d
bytekeeper <therealdante@gmail.com>
2018-12-27 21:46:30 +0100

    Formatting.

--- src/main/java/bwapi/EventType.java
+++ src/main/java/bwapi/EventType.java
@@ -3,37 +3,37 @@ package bwapi;
 import java.util.Arrays;
 
 public enum EventType {
-  MatchStart(0),
-  MatchEnd(1),
-  MatchFrame(2),
-  MenuFrame(3),
-  SendText(4),
-  ReceiveText(5),
-  PlayerLeft(6),
-  NukeDetect(7),
-  UnitDiscover(8),
-  UnitEvade(9),
-  UnitShow(10),
-  UnitHide(11),
-  UnitCreate(12),
-  UnitDestroy(13),
-  UnitMorph(14),
-  UnitRenegade(15),
-  SaveGame(16),
-  UnitComplete(17),
-  //TriggerAction,
-  None(18);
+    MatchStart(0),
+    MatchEnd(1),
+    MatchFrame(2),
+    MenuFrame(3),
+    SendText(4),
+    ReceiveText(5),
+    PlayerLeft(6),
+    NukeDetect(7),
+    UnitDiscover(8),
+    UnitEvade(9),
+    UnitShow(10),
+    UnitHide(11),
+    UnitCreate(12),
+    UnitDestroy(13),
+    UnitMorph(14),
+    UnitRenegade(15),
+    SaveGame(16),
+    UnitComplete(17),
+    //TriggerAction,
+    None(18);
 
-  static final EventType[] idToEnum = new EventType[19];
+    static final EventType[] idToEnum = new EventType[19];
 
-  static {
-    Arrays.stream(EventType.values()).forEach(v -> idToEnum[v.id] = v);
-  }
+    static {
+        Arrays.stream(EventType.values()).forEach(v -> idToEnum[v.id] = v);
+    }
 
-  final int id;
+    final int id;
 
-  EventType(int id) {
+    EventType(int id) {
 
-    this.id = id;
-  }
+        this.id = id;
+    }
 }
