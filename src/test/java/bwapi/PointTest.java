02e8135b8f7e4414470bff071ff3987b982486c3
Jasper Geurtz <jcgeurtz@gmail.com>
2020-04-20 08:56:27 +0200

    more point tests

--- src/test/java/bwapi/PointTest.java
+++ src/test/java/bwapi/PointTest.java
@@ -2,8 +2,11 @@ package bwapi;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
+import java.util.Random;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 public class PointTest {
@@ -47,6 +50,82 @@ public class PointTest {
 		assertEquals(wp, new WalkPosition(new Position(wp)));
 		assertEquals(wp.toTilePosition(), new TilePosition(wp));
 		assertEquals(wp, new WalkPosition(wp));
+	}
 
+	@Test
+	public void isValidChecks() {
+		Game game = mock(Game.class);
+		when(game.mapPixelWidth()).thenReturn(32 * 256);
+		when(game.mapPixelHeight()).thenReturn(32 * 256);
+
+		assertTrue(new Position(0,0).isValid(game));
+		assertTrue(new Position(1, 1).isValid(game));
+
+		assertFalse(new Position(-1, -1).isValid(game));
+		assertFalse(new Position(1, -1).isValid(game));
+
+		assertTrue(new TilePosition(0, 0).isValid(game));
+		assertFalse(new TilePosition(256, 256).isValid(game));
+	}
+
+	@Test
+	public void randomPointTests() {
+		Random r = new Random();
+		int bound = 100_000;
+
+		for (int i=0; i < 100; i++) {
+			TilePosition tp1 = new TilePosition(r.nextInt(bound), r.nextInt(bound));
+			TilePosition tp2 = new TilePosition(tp1);
+			assertEquals(tp1, tp2);
+
+			Position p1 = new Position(tp1);
+			Position p2 = tp1.toPosition();
+			assertEquals(p1, p2);
+			assertEquals(tp1.x * 32, p1.x);
+			assertEquals(tp1.y * 32 , p2.y);
+
+			WalkPosition wp1 = new WalkPosition(tp1);
+			WalkPosition wp2 = tp1.toWalkPosition();
+			assertEquals(wp1, wp2);
+			assertEquals(tp1.x * 4, wp1.x);
+			assertEquals(tp1.y * 4 , wp2.y);
+
+		}
+
+		for (int i=0; i < 100; i++) {
+			WalkPosition wp1 = new WalkPosition(r.nextInt(bound), r.nextInt(bound));
+			WalkPosition wp2 = new WalkPosition(wp1);
+			assertEquals(wp1, wp2);
+
+			Position p1 = new Position(wp1);
+			Position p2 = wp1.toPosition();
+			assertEquals(p1, p2);
+			assertEquals(wp1.x * 8, p1.x);
+			assertEquals(wp1.y * 8 , p2.y);
+
+			TilePosition tp1 = new TilePosition(wp1);
+			TilePosition tp2 = wp1.toTilePosition();
+			assertEquals(tp1, tp2);
+			assertEquals(wp1.x / 4, tp1.x);
+			assertEquals(wp1.y / 4 , tp2.y);
+		}
+
+		for (int i=0; i < 100; i++) {
+			Position p1 = new Position(r.nextInt(bound), r.nextInt(bound));
+			Position p2 = new Position(p1);
+			assertEquals(p1, p2);
+
+			WalkPosition wp1 = new WalkPosition(p1);
+			WalkPosition wp2 = p1.toWalkPosition();
+			assertEquals(wp1, wp2);
+			assertEquals(p1.x / 8, wp1.x);
+			assertEquals(p1.y / 8 , wp2.y);
+
+			TilePosition tp1 = new TilePosition(p1);
+			TilePosition tp2 = p1.toTilePosition();
+			assertEquals(tp1, tp2);
+			assertEquals(p1.x / 32, tp1.x);
+			assertEquals(p1.y / 32 , tp2.y);
+		}
 	}
 }
